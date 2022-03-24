8b4b290ec919987539a4163679af4d7705c21f92
melinepoghosyan <meline.poghosyan@picsart.com>
2022-03-24 18:18:42 +0400

    first commit

---
+++ src/components/patterns/ProductList/ProductList.js
@@ -0,0 +1,16 @@
+export const statusTypes = {
+  loading: "loading",
+  errored: "errored",
+  loaded: "loaded",
+};
+
+export default function ProductList({ status, ...otherProps
+                                    }) {
+  // if (status === statusTypes.loading) {
+  //   return <Loading />;
+  // }
+  // if (status === statusTypes.errored) {
+  //   return <Error message="Failed to load data" />;
+  // }
+  //   return ... // standard output when data present
+}
