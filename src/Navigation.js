ab14131e6113cbc865625e011036796751096aa4
melinepoghosyan <meline.poghosyan@picsart.com>
2022-04-10 22:38:45 +0400

    Navigation for storybook

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,14 +1,29 @@
-import { Link } from 'react-router-dom';
-export default function Navigation () {
+// import { Link } from 'react-router-dom';
+// export default function Navigation () {
+//   return (
+//     <nav>
+//       <ul>
+//         <li>
+//           <Link to="/">Home</Link> </li>
+//         <li>
+//           <Link to="/about">About</Link>
+//         </li>
+//       </ul>
+//     </nav>
+//   );
+// };
+
+import React from "react";
+import { Link } from "react-router-dom";
+export default function Navigation() {
   return (
     <nav>
-      <ul>
+      <ul> <li>
+        <Link to="/">Products</Link> </li>
         <li>
-          <Link to="/">Home</Link> </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-    </nav>
+          <Link to="/cart">Cart</Link>
+        </li> <li>
+          <Link to="/userProfile">Profile</Link> </li>
+      </ul> </nav>
   );
-};
+}
--- src/Routes.js
+++ src/Routes.js
@@ -3,9 +3,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import Cart from "./screens/Cart";
 import Products from "./screens/Products";
 // import UserProfile from "./screens/UserProfile";
+import Navigation from "./Navigation";
 export default function RoutesFor() {
   return (
     <BrowserRouter>
+      <Navigation />
       <Routes>
         {/*<Route path="/cart" component={Cart} />*/}
         {/*<Route path="/userProfile" component={UserProfile} />*/}
